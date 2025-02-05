75111b8817eb85f0885936ef6cfa4d38d5e7a864
4rthurV <arthur.vignes@epitech.eu>
2025-02-05 14:33:02 +0100

    fix(clean.c): add sfMusic_destroy for every sound

--- src/clean.c
+++ src/clean.c
@@ -7,22 +7,42 @@
 
 #include "../include/duck_hunt.h"
 
-void clean(game_t *game, sprite_t *sprite, mouse_t *mouse, button_t *button)
+void clean_sounds(game_t *game)
 {
-    if (game->music)
-        sfMusic_destroy(game->music);
+    sfMusic_destroy(game->music);
+    sfMusic_destroy(game->coin);
+    sfMusic_destroy(game->loose);
+    sfMusic_destroy(game->OneUp);
+    sfMusic_destroy(game->koopa);
+}
+
+void clean_sprites(game_t *game, sprite_t *sprite,
+    mouse_t *mouse, button_t *button)
+{
     sfSprite_destroy(sprite->sprite);
-    sfTexture_destroy(sprite->texture);
     sfSprite_destroy(mouse->mouse);
-    sfTexture_destroy(mouse->target);
     sfSprite_destroy(button->play);
-    sfTexture_destroy(button->playbutton);
     sfSprite_destroy(button->menu);
-    sfTexture_destroy(button->menubutton);
     sfSprite_destroy(game->image);
     sfSprite_destroy(sprite->life_bar);
+}
+
+void clean_textures(game_t *game, sprite_t *sprite,
+    mouse_t *mouse, button_t *button)
+{
+    sfTexture_destroy(sprite->texture);
+    sfTexture_destroy(mouse->target);
+    sfTexture_destroy(button->playbutton);
+    sfTexture_destroy(button->menubutton);
     sfTexture_destroy(sprite->one_ups);
     sfTexture_destroy(game->background);
+}
+
+void clean(game_t *game, sprite_t *sprite, mouse_t *mouse, button_t *button)
+{
+    clean_sounds(game);
+    clean_sprites(game, sprite, mouse, button);
+    clean_textures(game, sprite, mouse, button);
     sfClock_destroy(game->clock);
     sfRenderWindow_destroy(game->window);
     free(game);
